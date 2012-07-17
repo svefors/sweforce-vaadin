e592cf593ecf75e19e0dd7b213638fc2ff44357c
Mats Svefors <mats@sweforce.com>
2012-07-17 20:28:41 +0200

    PlaceControllertest

--- activitiesplaces/src/test/java/sweforce/gui/event/UmbrellaExceptionTest.java
+++ activitiesplaces/src/test/java/sweforce/gui/event/UmbrellaExceptionTest.java
@@ -1,5 +1,7 @@
 package sweforce.gui.event;
 
+import org.junit.Test;
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -14,60 +16,62 @@ import static org.junit.Assert.*;
  * To change this template use File | Settings | File Templates.
  */
 public class UmbrellaExceptionTest {
-
+    @Test
     public void testNone() {
-    // Why?
-    try {
-      throw new UmbrellaException(Collections.<Throwable> emptySet());
-    } catch (UmbrellaException e) {
-      assertNull(e.getCause());
-      assertNull(e.getMessage());
+        // Why?
+        try {
+            throw new UmbrellaException(Collections.<Throwable>emptySet());
+        } catch (UmbrellaException e) {
+            assertNull(e.getCause());
+            assertNull(e.getMessage());
+        }
     }
-  }
 
-  public void testOne() {
-    Set<Throwable> causes = new HashSet<Throwable>();
-    String message = "Just me";
-    RuntimeException theOne = new RuntimeException(message);
-    causes.add(theOne);
+    @Test
+    public void testOne() {
+        Set<Throwable> causes = new HashSet<Throwable>();
+        String message = "Just me";
+        RuntimeException theOne = new RuntimeException(message);
+        causes.add(theOne);
 
-    try {
-      throw new UmbrellaException(causes);
-    } catch (UmbrellaException e) {
-      assertSame(theOne, e.getCause());
-      assertEquals(UmbrellaException.ONE + message, e.getMessage());
+        try {
+            throw new UmbrellaException(causes);
+        } catch (UmbrellaException e) {
+            assertSame(theOne, e.getCause());
+            assertEquals(UmbrellaException.ONE + message, e.getMessage());
+        }
     }
-  }
 
-  public void testSome() {
-    Set<Throwable> causes = new HashSet<Throwable>();
-    String oneMessage = "one";
-    RuntimeException oneException = new RuntimeException(oneMessage);
-    causes.add(oneException);
+    @Test
+    public void testSome() {
+        Set<Throwable> causes = new HashSet<Throwable>();
+        String oneMessage = "one";
+        RuntimeException oneException = new RuntimeException(oneMessage);
+        causes.add(oneException);
 
-    String twoMessage = "two";
-    RuntimeException twoException = new RuntimeException(twoMessage);
-    causes.add(twoException);
+        String twoMessage = "two";
+        RuntimeException twoException = new RuntimeException(twoMessage);
+        causes.add(twoException);
 
-    try {
-      throw new UmbrellaException(causes);
-    } catch (UmbrellaException e) {
-      // A bit non-deterministic for a unit test, but I've checked both paths --
-      // rjrjr
-      if (e.getCause() == oneException) {
-        assertCauseMatchesFirstMessage(e, oneMessage, twoMessage);
-      } else if (e.getCause() == twoException) {
-        assertCauseMatchesFirstMessage(e, twoMessage, oneMessage);
-      } else {
-        fail("Expected one of the causes and its message");
-      }
+        try {
+            throw new UmbrellaException(causes);
+        } catch (UmbrellaException e) {
+            // A bit non-deterministic for a unit test, but I've checked both paths --
+            // rjrjr
+            if (e.getCause() == oneException) {
+                assertCauseMatchesFirstMessage(e, oneMessage, twoMessage);
+            } else if (e.getCause() == twoException) {
+                assertCauseMatchesFirstMessage(e, twoMessage, oneMessage);
+            } else {
+                fail("Expected one of the causes and its message");
+            }
+        }
     }
-  }
 
-  private void assertCauseMatchesFirstMessage(UmbrellaException e, String firstMessage,
-      String otherMessage) {
-    assertTrue("Cause should be first message", e.getMessage().startsWith(
-        2 + UmbrellaException.MULTIPLE + firstMessage));
-    assertTrue("Should also see the other message", e.getMessage().contains(otherMessage));
-  }
+    private void assertCauseMatchesFirstMessage(UmbrellaException e, String firstMessage,
+                                                String otherMessage) {
+        assertTrue("Cause should be first message", e.getMessage().startsWith(
+                2 + UmbrellaException.MULTIPLE + firstMessage));
+        assertTrue("Should also see the other message", e.getMessage().contains(otherMessage));
+    }
 }
